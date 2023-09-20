7c4aa755a6de1326bf8fc84e86412e96e05ca557
= <=>
2023-09-20 09:02:12 +0200

    first-commit

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        NewJFrame f = new NewJFrame();
+        f.setLocationRelativeTo(null);
+        f.setVisible(true);
+    }
+
+}
